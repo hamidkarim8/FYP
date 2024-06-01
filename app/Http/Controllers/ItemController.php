709bd998dc6e43947babde5785e778b90963cc2d
hamidkarim8 <hamidkarim2002@gmail.com>
2024-06-01 20:48:59 +0800

    setup controllers done

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class NotificationController extends Controller
+class ItemController extends Controller
 {
     /**
      * Display a listing of the resource.
