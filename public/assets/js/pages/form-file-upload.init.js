15259494c00442e5dfca89b4b2aa40394eaceb3a
hamidkarim8 <hamidkarim2002@gmail.com>
2024-06-13 20:16:38 +0800

    update

--- public/assets/js/pages/form-file-upload.init.js
+++ public/assets/js/pages/form-file-upload.init.js
@@ -3,63 +3,72 @@ var __webpack_exports__ = {};
 /*!*****************************************************!*\
   !*** ./resources/js/pages/form-file-upload.init.js ***!
   \*****************************************************/
-/*
-Template Name: Velzon - Admin & Dashboard Template
-Author: Themesbrand
-Website: https://Themesbrand.com/
-Contact: Themesbrand@gmail.com
-File: Form file upload Js File
-*/
-// Dropzone
-// var dropzonePreviewNode = document.querySelector("#dropzone-preview-list");
-// dropzonePreviewNode.id = "";
+/******/
+(function () {
+  // webpackBootstrap
+  var __webpack_exports__ = {};
+  /*!*****************************************************!*\
+    !*** ./resources/js/pages/form-file-upload.init.js ***!
+    \*****************************************************/
 
-// if (dropzonePreviewNode) {
-//   var previewTemplate = dropzonePreviewNode.parentNode.innerHTML;
-//   dropzonePreviewNode.parentNode.removeChild(dropzonePreviewNode);
-//   var dropzone = new Dropzone(".dropzone", {
-//     url: 'https://httpbin.org/post',
-//     method: "post",
-//     previewTemplate: previewTemplate,
-//     previewsContainer: "#dropzone-preview"
-//   });
-// } // FilePond
+  /*
+  Template Name: Velzon - Admin & Dashboard Template
+  Author: Themesbrand
+  Website: https://Themesbrand.com/
+  Contact: Themesbrand@gmail.com
+  File: Form file upload Js File
+  */
+  // Dropzone
+  // var dropzonePreviewNode = document.querySelector("#dropzone-preview-list");
+  // dropzonePreviewNode.id = "";
+  // if (dropzonePreviewNode) {
+  //   var previewTemplate = dropzonePreviewNode.parentNode.innerHTML;
+  //   dropzonePreviewNode.parentNode.removeChild(dropzonePreviewNode);
+  //   var dropzone = new Dropzone(".dropzone", {
+  //     url: 'https://httpbin.org/post',
+  //     method: "post",
+  //     previewTemplate: previewTemplate,
+  //     previewsContainer: "#dropzone-preview"
+  //   });
+  // } // FilePond
 
+  FilePond.registerPlugin( // encodes the file as base64 data
+  FilePondPluginFileEncode, // validates the size of the file
+  FilePondPluginFileValidateSize, // corrects mobile image orientation
+  FilePondPluginImageExifOrientation, // previews dropped images
+  FilePondPluginImagePreview);
+  var inputMultipleElements = document.querySelectorAll('input[type="file"].filepond');
+  var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-FilePond.registerPlugin( // encodes the file as base64 data
-FilePondPluginFileEncode, // validates the size of the file
-FilePondPluginFileValidateSize, // corrects mobile image orientation
-FilePondPluginImageExifOrientation, // previews dropped images
-FilePondPluginImagePreview);
-var inputMultipleElements = document.querySelectorAll('input[type="file"].filepond');
-const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  if (inputMultipleElements) {
+    // loop over input elements
+    Array.from(inputMultipleElements).forEach(function (inputElement) {
+      // create a FilePond instance at the input element location
+      FilePond.create(inputElement).setOptions({
+        server: {
+          process: '/uploads',
+          headers: {
+            'X-CSRF-TOKEN': csrfToken
+          }
+        },
+        allowMultiple: true
+      });
+    });
+    FilePond.create(document.querySelector('.filepond-input-circle'), {
+      labelIdle: 'Drag & Drop your picture or <span class="filepond--label-action">Browse</span>',
+      imagePreviewHeight: 170,
+      imageCropAspectRatio: '1:1',
+      imageResizeTargetWidth: 200,
+      imageResizeTargetHeight: 200,
+      stylePanelLayout: 'compact circle',
+      styleLoadIndicatorPosition: 'center bottom',
+      styleProgressIndicatorPosition: 'right bottom',
+      styleButtonRemoveItemPosition: 'left bottom',
+      styleButtonProcessItemPosition: 'right bottom'
+    });
+  }
+  /******/
 
-if (inputMultipleElements) {
-  // loop over input elements
-  Array.from(inputMultipleElements).forEach(function (inputElement) {
-    // create a FilePond instance at the input element location
-    FilePond.create(inputElement).setOptions({
-      server: {
-        process: '/uploads',
-        headers: {
-            'X-CSRF-TOKEN': csrfToken,
-        }
-    },
-    allowMultiple: true,
-  });
-  });
-  FilePond.create(document.querySelector('.filepond-input-circle'), {
-    labelIdle: 'Drag & Drop your picture or <span class="filepond--label-action">Browse</span>',
-    imagePreviewHeight: 170,
-    imageCropAspectRatio: '1:1',
-    imageResizeTargetWidth: 200,
-    imageResizeTargetHeight: 200,
-    stylePanelLayout: 'compact circle',
-    styleLoadIndicatorPosition: 'center bottom',
-    styleProgressIndicatorPosition: 'right bottom',
-    styleButtonRemoveItemPosition: 'left bottom',
-    styleButtonProcessItemPosition: 'right bottom'
-  });
-}
+})();
 /******/ })()
 ;
--- resources/js/pages/form-file-upload.init.js
+++ resources/js/pages/form-file-upload.init.js
@@ -1,48 +1,54 @@
-/*
-Template Name: Velzon - Admin & Dashboard Template
-Author: Themesbrand
-Website: https://Themesbrand.com/
-Contact: Themesbrand@gmail.com
-File: Form file upload Js File
-*/
-
-// Dropzone
-var dropzonePreviewNode = document.querySelector("#dropzone-preview-list");
-dropzonePreviewNode.id = "";
-if(dropzonePreviewNode){
-    var previewTemplate = dropzonePreviewNode.parentNode.innerHTML;
-    dropzonePreviewNode.parentNode.removeChild(dropzonePreviewNode);
-    var dropzone = new Dropzone(".dropzone", {
-        url: 'https://httpbin.org/post',
-        method: "post",
-        previewTemplate: previewTemplate,
-        previewsContainer: "#dropzone-preview",
-    });
-}
-
-// FilePond
-FilePond.registerPlugin(
-    // encodes the file as base64 data
-    FilePondPluginFileEncode,
-    // validates the size of the file
-    FilePondPluginFileValidateSize,
-    // corrects mobile image orientation
-    FilePondPluginImageExifOrientation,
-    // previews dropped images
-    FilePondPluginImagePreview
-);
-
-var inputMultipleElements = document.querySelectorAll('input.filepond-input-multiple');
-if(inputMultipleElements){
-
-// loop over input elements
-Array.from(inputMultipleElements).forEach(function (inputElement) {
-    // create a FilePond instance at the input element location
-    FilePond.create(inputElement);
-})
-
-FilePond.create(
-    document.querySelector('.filepond-input-circle'), {
+/******/ (() => { // webpackBootstrap
+    var __webpack_exports__ = {};
+    /*!*****************************************************!*\
+      !*** ./resources/js/pages/form-file-upload.init.js ***!
+      \*****************************************************/
+    /*
+    Template Name: Velzon - Admin & Dashboard Template
+    Author: Themesbrand
+    Website: https://Themesbrand.com/
+    Contact: Themesbrand@gmail.com
+    File: Form file upload Js File
+    */
+    // Dropzone
+    // var dropzonePreviewNode = document.querySelector("#dropzone-preview-list");
+    // dropzonePreviewNode.id = "";
+    
+    // if (dropzonePreviewNode) {
+    //   var previewTemplate = dropzonePreviewNode.parentNode.innerHTML;
+    //   dropzonePreviewNode.parentNode.removeChild(dropzonePreviewNode);
+    //   var dropzone = new Dropzone(".dropzone", {
+    //     url: 'https://httpbin.org/post',
+    //     method: "post",
+    //     previewTemplate: previewTemplate,
+    //     previewsContainer: "#dropzone-preview"
+    //   });
+    // } // FilePond
+    
+    
+    FilePond.registerPlugin( // encodes the file as base64 data
+    FilePondPluginFileEncode, // validates the size of the file
+    FilePondPluginFileValidateSize, // corrects mobile image orientation
+    FilePondPluginImageExifOrientation, // previews dropped images
+    FilePondPluginImagePreview);
+    var inputMultipleElements = document.querySelectorAll('input[type="file"].filepond');
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    
+    if (inputMultipleElements) {
+      // loop over input elements
+      Array.from(inputMultipleElements).forEach(function (inputElement) {
+        // create a FilePond instance at the input element location
+        FilePond.create(inputElement).setOptions({
+          server: {
+            process: '/uploads',
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+            }
+        },
+        allowMultiple: true,
+      });
+      });
+      FilePond.create(document.querySelector('.filepond-input-circle'), {
         labelIdle: 'Drag & Drop your picture or <span class="filepond--label-action">Browse</span>',
         imagePreviewHeight: 170,
         imageCropAspectRatio: '1:1',
@@ -52,7 +58,8 @@ FilePond.create(
         styleLoadIndicatorPosition: 'center bottom',
         styleProgressIndicatorPosition: 'right bottom',
         styleButtonRemoveItemPosition: 'left bottom',
-        styleButtonProcessItemPosition: 'right bottom',
+        styleButtonProcessItemPosition: 'right bottom'
+      });
     }
-);
-}
+    /******/ })()
+    ;
