3427ca35540448f493093118e70c920861ac0c07
hamidkarim8 <hamidkarim2002@gmail.com>
2024-07-14 23:35:30 +0800

    notificaiton send email msg

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,21 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
 use App\Notifications\ContactNotification;
+use Illuminate\Validation\ValidationException;
 
 class ContactController extends Controller
 {
     public function sendEmail(Request $request)
     {
-        $data = [
-            'name' => $request->name,
-            'email' => $request->email,
-            'subject' => $request->subject,
-            'comments' => $request->comments,
-        ];
+        try{
+            $data = [
+                'name' => $request->name,
+                'email' => $request->email,
+                'subject' => $request->subject,
+                'comments' => $request->comments,
+            ];
+    
+            Notification::route('mail', 'hamidkarim2002@gmail.com')
+                ->notify(new ContactNotification($data));
+    
+            return redirect()->back()->with('success', 'Your message has been sent!');
+        } catch(ValidationException $e){
+            $errorMessage = $e->getMessage();
+            return redirect()->back()->with('error', $errorMessage);
+        }
 
-        Notification::route('mail', 'hamidkarim2002@gmail.com')
-            ->notify(new ContactNotification($data));
-
-        return redirect()->back()->with('message', 'Your message has been sent!');
     }
 }
